a764292bfc78e40e6d2ef04008bc1d1ca151d02a
Ashique <ashique9496@gmail.com>
2025-11-25 16:32:14 +0530

    Endpoint added in sitemap

--- frontend/src/app/sitemap.js
+++ frontend/src/app/sitemap.js
@@ -1,14 +1,18 @@
 export default function sitemap() {
-  const baseUrl = "https://cozech.com";
+    const baseUrl = "https://cozech.com";
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/pricing-policy`,
-      lastModified: new Date(),
-    },
-  ];
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/pricing-policy`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/jump-start`,
+            lastModified: new Date(),
+        },
+    ];
 }
